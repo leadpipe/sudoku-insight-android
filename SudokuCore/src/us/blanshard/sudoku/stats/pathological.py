c22c12e0753a4b2e02b644923d4fb42f426fc416
leadpipe@localhost <leadpipe@localhost>
2011-12-04 21:11:50 -0600

    Find truly pathological grids from among the slow ones.

---
+++ SudokuCore/src/us/blanshard/sudoku/stats/pathological.py
@@ -0,0 +1,34 @@
+# Copyright 2011 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Filters out the sporadically slow leaving the truly pathological."""
+
+import sys
+
+def main():
+  results = []
+  for line in sys.stdin:
+    fields = line.split("\t")
+    steps = [int(fields[i]) for i in range(3, len(fields), 2)]
+    slow_count = sum([s > 32 for s in steps])
+    if slow_count > 2:
+      avg_slow = sum([s for s in steps if s > 32]) / slow_count
+      results.append((slow_count, avg_slow, fields[0], fields[1], fields[2]))
+
+  for result in sorted(results, reverse=True):
+    print "{}\t{}\t{}\t{}\t{}".format(*result)
+
+
+if __name__ == "__main__":
+  main()
