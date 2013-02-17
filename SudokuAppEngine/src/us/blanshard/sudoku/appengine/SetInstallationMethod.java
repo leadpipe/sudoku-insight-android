3fc2ad81be50328f0f7c5d1b74c188390aa8ed9d
Luke Blanshard <leadpipe@google.com>
2013-02-17 12:57:33 -0600

    Add account logic.

--- SudokuAppEngine/src/us/blanshard/sudoku/appengine/SetInstallationMethod.java
+++ SudokuAppEngine/src/us/blanshard/sudoku/appengine/SetInstallationMethod.java
@@ -16,11 +16,13 @@ limitations under the License.
 package us.blanshard.sudoku.appengine;
 
 import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
 import static us.blanshard.sudoku.appengine.Schema.Installation.ACCOUNT_ID;
 import static us.blanshard.sudoku.appengine.Schema.Installation.INDEXED_ID;
 import static us.blanshard.sudoku.appengine.Schema.Installation.KIND;
 import static us.blanshard.sudoku.appengine.Schema.Installation.MANUFACTURER;
 import static us.blanshard.sudoku.appengine.Schema.Installation.MODEL;
+import static us.blanshard.sudoku.appengine.Schema.Installation.MONTH_NUMBER;
 import static us.blanshard.sudoku.appengine.Schema.Installation.NAME;
 import static us.blanshard.sudoku.appengine.Schema.Installation.OPAQUE_ID;
 import static us.blanshard.sudoku.appengine.Schema.Installation.STREAM;
@@ -42,14 +44,17 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Transaction;
-import com.google.appengine.api.users.User;
+import com.google.appengine.api.datastore.TransactionOptions;
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -66,6 +71,7 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
   private final JsonFactory jsonFactory = new GsonFactory();
   private final GoogleIdTokenVerifier tokenVerifier = new GoogleIdTokenVerifier(new NetHttpTransport(), jsonFactory);
   private Iterator<Key> opaqueIds;
+  private Iterator<Key> opaqueAccountIds;
 
 
   public SetInstallationMethod() {
@@ -75,7 +81,8 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
   @Override public UpdateResult call(UpdateParams params) throws MethodException {
 
     DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    Transaction tx = ds.beginTransaction();
+    TransactionOptions options = TransactionOptions.Builder.withXG(true);
+    Transaction tx = ds.beginTransaction(options);
     boolean update = true;
     Long opaqueId;
     try {
@@ -98,12 +105,16 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
         entity.setUnindexedProperty(OPAQUE_ID, opaqueId);
       }
 
+      String oldAccountId = (String) entity.getProperty(ACCOUNT_ID);
+      String newAccountId = null;
+
       if (params.account == null) {
         entity.removeProperty(ACCOUNT_ID);
         entity.removeProperty(NAME);
       } else {
-        check(params.account.authToken, params.account.id);
-        entity.setProperty(ACCOUNT_ID, new User(params.account.id, "gmail.com"));
+        checkAuth(params.account.authToken, params.account.id);
+        newAccountId = params.account.id;
+        entity.setProperty(ACCOUNT_ID, newAccountId);
         entity.setUnindexedProperty(NAME, params.account.installationName);
       }
 
@@ -118,6 +129,12 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
       entity.setUnindexedProperty(MODEL, params.model);
       entity.setUnindexedProperty(STREAM_COUNT, params.streamCount);
       entity.setUnindexedProperty(STREAM, params.stream);
+      entity.setUnindexedProperty(MONTH_NUMBER, params.monthNumber);
+
+      if (!Objects.equal(oldAccountId, newAccountId)) {
+        removeInstallationFromAccount(oldAccountId, ds, tx, params, opaqueId);
+        addInstallationToAccount(newAccountId, ds, tx, params);
+      }
 
       ds.put(tx, entity);
       tx.commit();
@@ -128,8 +145,14 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
     logger.info((update ? "Updated" : "Inserted") + " installation " + opaqueId);
 
     UpdateResult answer = new UpdateResult();
+    // TODO: look for clashing installation names, rectify
+    // TODO: look for clashing streams, choose a different one
+    // only look in active installations, ie ones used within the past couple of months
+    // TODO: if implied stream count > given, send email?  or log so I'll notice
     answer.streamCount = params.streamCount;
     answer.stream = params.stream;
+    if (params.account != null)
+      answer.installationName = params.account.installationName;
     return answer;
   }
 
@@ -144,12 +167,19 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
 
   private synchronized long nextOpaqueId(DatastoreService ds) {
     if (opaqueIds == null || !opaqueIds.hasNext()) {
-      opaqueIds = ds.allocateIds(Schema.Installation.KIND, 20).iterator();
+      opaqueIds = ds.allocateIds(KIND, 20).iterator();
     }
     return opaqueIds.next().getId();
   }
 
-  private void check(String tokenString, String email) throws MethodException {
+  private synchronized long nextOpaqueAccountId(DatastoreService ds) {
+    if (opaqueAccountIds == null || !opaqueAccountIds.hasNext()) {
+      opaqueAccountIds = ds.allocateIds(Schema.Account.KIND, 10).iterator();
+    }
+    return opaqueAccountIds.next().getId();
+  }
+
+  private void checkAuth(String tokenString, String email) throws MethodException {
     GoogleIdToken token = null;
     boolean ok = false;
     try {
@@ -169,5 +199,50 @@ public class SetInstallationMethod extends RpcMethod<UpdateParams, UpdateResult>
       logger.info("Auth verification failed for " + token);
       throw new MethodException(Rpc.error(Rpc.AUTH_VERIFICATION_FAILED, "Verification failed", null));
     }
+  }
+
+  private void removeInstallationFromAccount(String accountId, DatastoreService ds,
+      Transaction tx, UpdateParams params, Long opaqueId) {
+    if (accountId == null)
+      return;
+
+    Key accountKey = KeyFactory.createKey(Schema.Account.KIND, accountId);
+    try {
+      Entity account = ds.get(accountKey);
+      Set<String> ids = getInstallationIds(account);
+      ids.remove(params.id);
+      account.setUnindexedProperty(Schema.Account.INSTALLATION_IDS, ids);
+      ds.put(tx, account);
+    } catch (EntityNotFoundException e) {
+      logger.log(WARNING, "Old account not found: " + accountId
+          + ", for " + opaqueId);
+    }
+  }
+
+  private void addInstallationToAccount(String accountId, DatastoreService ds,
+      Transaction tx, UpdateParams params) {
+    if (accountId == null)
+      return;
+
+    Key accountKey = KeyFactory.createKey(Schema.Account.KIND, accountId);
+    Entity account;
+    Set<String> ids;
+    try {
+      account = ds.get(accountKey);
+      ids = getInstallationIds(account);
+    } catch (EntityNotFoundException e) {
+      account = new Entity(accountKey);
+      account.setProperty(OPAQUE_ID, nextOpaqueAccountId(ds));
+      ids = Sets.newLinkedHashSet();
+    }
+    ids.add(params.id);
+    account.setUnindexedProperty(Schema.Account.INSTALLATION_IDS, ids);
+    ds.put(tx, account);
+  }
+
+  @SuppressWarnings("unchecked")
+  private Set<String> getInstallationIds(Entity account) {
+    return Sets.newLinkedHashSet(
+        (Collection<String>) account.getProperty(Schema.Account.INSTALLATION_IDS));
   }
 }
