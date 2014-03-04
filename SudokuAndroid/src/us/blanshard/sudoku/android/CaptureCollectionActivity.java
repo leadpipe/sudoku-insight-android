cd8c9c0acd7b57bfaac0c00d1ff6778eb25d69bf
Luke Blanshard <leadpipe@google.com>
2014-03-03 20:46:21 -0600

    Start work on supporting collections in the app

---
+++ SudokuAndroid/src/us/blanshard/sudoku/android/CaptureCollectionActivity.java
@@ -0,0 +1,12 @@
+package us.blanshard.sudoku.android;
+
+/**
+ * Captures a list of puzzles from a .sdm file.
+ */
+public class CaptureCollectionActivity extends ActivityBase {
+
+  @Override protected String getHelpPage() {
+    return "collection";
+  }
+
+}
